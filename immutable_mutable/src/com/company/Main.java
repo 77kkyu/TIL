9ccd144feb5c21db511d86f42463e2164297e8a2
77kkyu <77kkyu@gmail.com>
2022-01-15 21:21:00 +0900

    feat immutable Test

--- immutable_mutable/src/com/company/Main.java
+++ immutable_mutable/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String name = "해쉬";
+        name = name.concat(" 입니다"); // new String()
+        System.out.println(name);
+
+        System.out.println("===========================");
+
+        StringBuilder builder = new StringBuilder("에이든");
+        System.out.println("builder1 : " + builder.hashCode());
+        builder.append(" 입니다");
+        System.out.println("builder2 : " + builder.hashCode());
+        System.out.println(builder);
+
     }
 }
