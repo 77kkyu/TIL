99e249382c2387b3099a2a70193ab76b30893c89
77kkyu <77kkyu@gmail.com>
2022-04-16 21:14:23 +0900

    feat MemberService

---
+++ jpa_persistence_context/src/main/java/com/example/jpa_persistence_context/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.example.jpa_persistence_context.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+
+@Service
+public class MemberService {
+
+}
